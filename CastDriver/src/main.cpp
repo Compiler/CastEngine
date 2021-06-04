cf4d9d69cbe0c9291eef68255d5f109bff686431
compiler <lukerochedev@gmail.com>
2021-06-04 10:02:30 -0400

    fmt logging

--- CastDriver/src/main.cpp
+++ CastDriver/src/main.cpp
@@ -1,7 +1,9 @@
 #include <stdio.h>
-
+#include <Cast/Core/Debug/Logging.h>
+#define TESTER1(...) printf(ANSI_COLOR_BOLD_GREEN "LOG::" __FILE__ ":" STR(__LINE__) "\t" ANSI_COLOR_RESET ANSI_COLOR_GREEN __VA_ARGS__); printf(ANSI_COLOR_RESET "\n")
+#define TESTER(...) fmt::print(fmt::emphasis::bold | fg(fmt::color::red), __VA_ARGS__)
 int main(){
-
-    printf("Init\n");
+    std::string message = fmt::format("The answer is {}", 42);
+    TESTER("The answer is {}", 42);
 
 }
--- CastEngine/src/Cast/Core/Debug/Logging.h
+++ CastEngine/src/Cast/Core/Debug/Logging.h
@@ -1,6 +1,8 @@
 #pragma once
 #include <cassert>
-
+#include <fmt/core.h>
+#include <fmt/color.h>
+#include <fmt/format.h>
 #if __INTELLISENSE__
 #pragma diag_suppress 2486
 #endif
