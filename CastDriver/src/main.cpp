74c4b851651a51874024b7785c50c57d66654b90
compiler <lukerochedev@gmail.com>
2021-06-04 10:18:14 -0400

    fmt logging for user types

--- CastDriver/src/main.cpp
+++ CastDriver/src/main.cpp
@@ -1,9 +1,13 @@
 #include <stdio.h>
-#include <Cast/Core/Debug/Logging.h>
+#include <Cast/Core/Debug/LoggingHeaderOnly.h>
+#include <Cast/Core/Debug/Logger.h>
+#include <Cast/Core/Debug/LoggingUserFormatter.h>
+#include <glm/vec2.hpp>
+#include <glm/vec3.hpp>
+#include <glm/vec4.hpp>
 #define TESTER1(...) printf(ANSI_COLOR_BOLD_GREEN "LOG::" __FILE__ ":" STR(__LINE__) "\t" ANSI_COLOR_RESET ANSI_COLOR_GREEN __VA_ARGS__); printf(ANSI_COLOR_RESET "\n")
 #define TESTER(...) fmt::print(fmt::emphasis::bold | fg(fmt::color::red), __VA_ARGS__)
 int main(){
-    std::string message = fmt::format("The answer is {}", 42);
-    TESTER("The answer is {}", 42);
+    TESTER("glm::vec2 = {}\nglm::vec3 = {}\nglm::vec4 = {}\n", glm::vec2(1.2, 3.4), glm::vec3(1.2, 3.4, 5.6), glm::vec4(1.2, 3.4, 5.6, 7.8));
 
 }
---
+++ CastEngine/src/Cast/Core/Debug/Logger.h
@@ -0,0 +1,20 @@
+#pragma once
+#include <cassert>
+#include <fmt/core.h>
+#include <fmt/color.h>
+#include <fmt/format.h>
+#if __INTELLISENSE__
+#pragma diag_suppress 2486
+#endif    
+
+
+
+
+namespace Cast{
+
+    class Logger{
+        
+
+    };
+
+}
