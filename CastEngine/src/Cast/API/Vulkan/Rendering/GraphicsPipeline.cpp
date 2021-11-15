01ab1ba6d4fe541c960226af0427127374d3463f
unknown <lukerochedev@gmail.com>
2021-11-15 17:23:26 -0500

    3d done for vulkan

--- CastEngine/src/Cast/API/Vulkan/Rendering/GraphicsPipeline.cpp
+++ CastEngine/src/Cast/API/Vulkan/Rendering/GraphicsPipeline.cpp
@@ -72,7 +72,8 @@ namespace Cast{
         rasterizer.polygonMode = VK_POLYGON_MODE_FILL;
         rasterizer.lineWidth = 1.0f;
 
-        rasterizer.cullMode = VK_CULL_MODE_NONE;//VK_CULL_MODE_BACK_BIT;
+        //TODO: support bit culling for performance!
+        rasterizer.cullMode = VK_CULL_MODE_FRONT_BIT;//VK_CULL_MODE_NONE;
         rasterizer.frontFace = VK_FRONT_FACE_COUNTER_CLOCKWISE;
 
         rasterizer.depthBiasEnable = VK_FALSE;
